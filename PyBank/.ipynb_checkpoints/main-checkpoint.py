32f1f6c86c70012019a885bc10464dd7ad54f30b
Abesprog <abesprgrss@gmail.com>
2022-04-15 16:26:15 -0500

    git commit

--- PyBank/.ipynb_checkpoints/main-checkpoint.py
+++ PyBank/.ipynb_checkpoints/main-checkpoint.py
@@ -4,12 +4,6 @@ import csv
 
 budget_data = os.path.join("Resources", "budget_data.csv")
 
-totmon = []
-pltot = []
-plchg = 0.0
-
-
-
 # The total number of months included in the dataset
 
 # The net total amount of Profit/Losses over the entire period
@@ -26,10 +20,18 @@ plchg = 0.0
     #lines = text.read()
     #print(lines)
 
-with open(budget_data)as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    print(csvreader)
-    csv_header = next(csvreader, None)
-    print(f"CSV Header: {csv_header}")
-    for row in csvreader:
-        print(row)
+#with open(budget_data)as csvfile:
+    #csvreader = csv.reader(csvfile, delimiter=',')
+    #print(csvreader)
+    #csv_header = next(csvreader)
+    #print(f"CSV Header: {csv_header}")
+    #for row in csvreader:
+    #print(row)
+csv_file = csv.reader(open(budget_data))
+months = 0
+for row in csv_file:    
+    months = row[0]
+    months = str(months)
+    print(months)
+
+
